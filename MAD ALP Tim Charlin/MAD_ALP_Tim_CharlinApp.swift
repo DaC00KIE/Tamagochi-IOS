cbc6188d54ac0b932be9fef617ff29802cc2b75a
Gavin <ggavinlie@gmail.com>
2024-05-16 12:38:26 +0700

    Initial Commit

---
+++ MAD ALP Tim Charlin/MAD_ALP_Tim_CharlinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD_ALP_Tim_CharlinApp.swift
+//  MAD ALP Tim Charlin
+//
+//  Created by MacBook Pro on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MAD_ALP_Tim_CharlinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
