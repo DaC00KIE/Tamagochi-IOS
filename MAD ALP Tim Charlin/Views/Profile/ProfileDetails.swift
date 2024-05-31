bb737faf252586d1e2d93ac4923c730edf26fbfc
JDTOmoi <jasondavetahir@gmail.com>
2024-05-31 09:18:24 +0700

    Basic Editor for Pet Profile (name only), navigation for initialization. Cannot proceed initialization further without Main Menu Implementation.

---
+++ MAD ALP Tim Charlin/Views/Profile/ProfileDetails.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileDetails.swift
+//  MAD ALP Tim Charlin
+//
+//  Created by student on 31/05/24.
+//
+
+import SwiftUI
+
+struct ProfileDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileDetails()
+}
