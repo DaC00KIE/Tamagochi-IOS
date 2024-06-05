eebc3bcb3955c868fdbd8c7a171588268316547a
Patrickstvn <168065344+Patrickstvn@users.noreply.github.com>
2024-06-06 00:31:56 +0700

    color picker working

--- MAD ALP Tim Charlin/Models/Tamagochi.swift
+++ MAD ALP Tim Charlin/Models/Tamagochi.swift
@@ -8,13 +8,7 @@
 import Foundation
 import SwiftUI
 
-//@State private var characterImage = UIImage(named: "character")! // Replace with your character image
-//@State private var faceImage: UIImage?
-//@State private var hatImage: UIImage?
-//@State private var selectedFace: String = "Default_face"
-//@State private var selectedHat: String = "none"
-
-class Tamagochi: ObservableObject{
+class Tamagochi: ObservableObject {
     var name: String
     
     @Published var health = Stat(value: 100, max: 100)
@@ -24,20 +18,19 @@ class Tamagochi: ObservableObject{
     @Published var energy = Stat(value: 1800, max: 3600)
     
     @Published var characterImage: UIImage
-    @Published var faceImage: UIImage? = UIImage(named:"Default_face")
-    @Published var hatImage: UIImage? = UIImage(named:"none")
+    @Published var faceImage: UIImage? = UIImage(named: "Default_face")
+    @Published var hatImage: UIImage? = UIImage(named: "none")
     @Published var selectedFace: String
     @Published var selectedHat: String
     
-    var skinColor: UIColor = .red
+    @Published var skinColor: Color
     
-    init(){
+    init() {
         self.name = "Default Name"
         self.characterImage = UIImage(named: "character")!
         self.selectedFace = "face_default"
         self.selectedHat = "hat_none"
-        
-        self.skinColor = UIColor.systemRed
+        self.skinColor = Color.red
     }
     
     func eat(amount: Int) {
@@ -46,65 +39,56 @@ class Tamagochi: ObservableObject{
 
     func clean(amount: Int) {
         cleanliness.add(amount)
-        
-        energy.minus(amount/4)
-        fun.minus(amount/2)
+        energy.minus(amount / 4)
+        fun.minus(amount / 2)
     }
 
     func play(amount: Int) {
         fun.add(amount)
-        
-        energy.minus(amount/2)
-        cleanliness.minus(amount/4)
-        
-        if hunger.value < amount/4{
+        energy.minus(amount / 2)
+        cleanliness.minus(amount / 4)
+        if hunger.value < amount / 4 {
             health.minus(5)
         }
-        hunger.minus(amount/4)
-        
+        hunger.minus(amount / 4)
     }
     
-    func rest(amount: Int){
+    func rest(amount: Int) {
         energy.add(amount)
-        hunger.minus(amount/4)
+        hunger.minus(amount / 4)
     }
     
-    func minusBars(by: Int){
+    func minusBars(by: Int) {
         hunger.minus(by)
         cleanliness.minus(by)
-        fun.minus(by/2)
-        energy.minus(by/2)
+        fun.minus(by / 2)
+        energy.minus(by / 2)
     }
 }
 
-struct Stat{
+struct Stat {
     var value: Int
     var max: Int
     
-    var percentage: Int{
+    var percentage: Int {
         guard max > 0 else { return 0 }
         let percentage = Double(value) / Double(max) * 100
         return Int(round(percentage))
     }
     
-    var isFull: Bool{
-        if value < max{
-            return false
-        }
-        return true
+    var isFull: Bool {
+        return value >= max
     }
     
-    mutating func add(_ amount: Int){
+    mutating func add(_ amount: Int) {
         value = min(value + amount, max)
     }
     
-    mutating func minus(_ amount: Int){
-        // not using max(value - amount, 0) because conflict w var name
+    mutating func minus(_ amount: Int) {
+        // not using max(value - amount, 0) because conflict with var name
         value -= amount
-        if value < 0{
+        if value < 0 {
             value = 0
         }
     }
-    
-
 }
--- MAD ALP Tim Charlin/Views/Helpers/TamagochiDisplay.swift
+++ MAD ALP Tim Charlin/Views/Helpers/TamagochiDisplay.swift
@@ -4,36 +4,47 @@
 //
 //  Created by MacBook Pro on 05/06/24.
 //
-
+//
+//  TamagochiDisplay.swift
+//  MAD ALP Tim Charlin
+//
+//  Created by Patrick Steven Kent Sugiarto on 05/06/24.
+//
 import SwiftUI
 
 struct TamagochiDisplay: View {
     @StateObject var tamagochi: Tamagochi
-    var frame: Int
-    private var frameSize: CGFloat{
-        return CGFloat(frame)
-    }
+      var frame: Int
+      private var frameSize: CGFloat{
+          return CGFloat(frame)
+      }
     
     var body: some View {
         ZStack {
             Image(uiImage: tamagochi.characterImage)
                 .resizable()
+//                .scaledToFit()
                 .frame(width: frameSize, height: frameSize)
+                .background(Color(tamagochi.skinColor))
+//                .clipShape(Circle())
+            
             if let faceImage = UIImage(named: "\(tamagochi.selectedFace)") {
-                Image(uiImage: faceImage)
-                    .resizable()
-                    .frame(width: frameSize, height: frameSize)
-            }
-            if let hatImage = UIImage(named: "\(tamagochi.selectedHat)") {
-                Image(uiImage: hatImage)
-                    .resizable()
-                    .frame(width: frameSize, height: frameSize)
-            }
+                            Image(uiImage: faceImage)
+                                .resizable()
+                                .frame(width: frameSize, height: frameSize)
+                        }
+                        if let hatImage = UIImage(named: "\(tamagochi.selectedHat)") {
+                            Image(uiImage: hatImage)
+                                .resizable()
+                                .frame(width: frameSize, height: frameSize)
+                        }
         }
     }
 }
 
-#Preview {
-    @StateObject var tamagochi = Tamagochi()
-    return TamagochiDisplay(tamagochi: tamagochi, frame: 400)
+struct TamagochiDisplay_Previews: PreviewProvider {
+    static var previews: some View {
+        let tamagochi = Tamagochi()
+        return TamagochiDisplay(tamagochi: tamagochi, frame: 500)
+    }
 }
