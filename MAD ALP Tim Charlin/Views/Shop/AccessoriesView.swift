14a428b75be18ad1de05b97e68d9e77d5b0780cf
charlinleo <ccharlin@student.ciputra.ac.id>
2024-06-06 12:51:59 +0700

    2

---
+++ MAD ALP Tim Charlin/Views/Shop/AccessoriesView.swift
@@ -0,0 +1,18 @@
+//
+//  AccessoriesView.swift
+//  MAD ALP Tim Charlin
+//
+//  Created by Charlin Leo on 06/06/24.
+//
+
+import SwiftUI
+
+struct AccessoriesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AccessoriesView()
+}
